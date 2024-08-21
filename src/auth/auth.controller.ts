7d80183b7d19f1de14bfdd8b2288ddb5ead4b6c8
quanghung309 <95295846+quanghung309@users.noreply.github.com>
2024-08-21 21:01:16 +0700

    login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Res, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local.auth.guard';
+import { CurrentUser } from './current-user.decorator';
+import { User } from '@prisma/client';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login() {}
+  login(
+    @CurrentUser() user: User,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    return this.authService.login(user, response);
+  }
 }
